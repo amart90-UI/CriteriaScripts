6e3d7b714af52a885782d2c519ecadd10066dc79
amart90 <anthonymartinez90@gmail.com>
2018-07-13 16:48:12 -0700

    Remove code that's already been done

--- CombinedCriteria.R
+++ CombinedCriteria.R
@@ -25,14 +25,6 @@ pnw <- readOGR("PNW/pnw.shp")
 fire.perim <- readOGR("UnburnedIsland/mtbs_perims_1984_2014.shp")
 ui <- readOGR("UnburnedIsland/unburned_areas.shp")
 
-# Fix UI - resave it
-ui <- spTransform(ui, proj4string(fire.perim))
-ui <- gBuffer(ui, byid=T, width=0)
-ui <- ui[order(match(as.numeric(as.character(ui@data$ID)),sort(as.numeric(as.character(ui@data$ID))))),]
-ui@data <- ui@data[, -2]
-writeOGR(ui, dsn= "UnburnedIsland", layer = "unburned_areas", driver = "ESRI Shapefile", overwrite_layer = T)
-
-
 scores.df <- data.frame(ID = ui@data$ID)
 
 

36c2a8d6cbe24386c942c15c4c6b34957c859437
amart90 <anthonymartinez90@gmail.com>
2018-07-19 12:06:31 -0700

    Major additions - Define criteria functions

--- CombinedCriteria.R
+++ CombinedCriteria.R
@@ -46,6 +46,12 @@ age <- crop(age, extent(fire.proj))
 
 # Mask stand age to UIs
 age.ui <- mask(age, ui.proj)
+writeRaster(age.ui, "StandAge.tif")
+age.ui <- raster("StandAge.tif")
+
+# Extract mean stand age values for each UI
+ui.age <- extract(age.ui, ui.proj, small=T, fun=mean, na.rm=T)
+ui.age[is.na(ui.age)] <- 0
 
 # Extract mean stand age values for each UI
 ui.age <- extract(age.ui, ui.proj, small=T, fun=mean, na.rm=T)
@@ -56,6 +62,35 @@ scores.df$score.age <- as.numeric(ui.age)
 ui@data$score.age <- as.numeric(ui.age)
 #writeOGR(ui, dsn= "StandAge", layer = "StandAge", driver = "ESRI Shapefile", overwrite_layer = T)
 writeRaster(age.ui, "StandAge.tif")
+age.ui <- raster("StandAge.tif")
+
+################
+
+# Define function to calculate mean stand age
+StandAge <- function(ui, fire.perim){
+  # Load packages
+  require(rgdal)
+  require(raster)
+  
+  # Load data
+  age <- raster("StandAge/StandAge.clip.tif")
+  
+  # Transform projection
+  fire.proj <- spTransform(fire.perim, projection(age))
+  ui.proj <- spTransform(ui, projection(age))
+  
+  # Clip to fire extent
+  age <- crop(age, extent(fire.proj))
+  
+  # Mask stand age to UIs
+  age.ui <- mask(age, ui.proj)
+  
+  # Extract mean stand age values for each UI
+  ui.age <- extract(age.ui, ui.proj, small=T, fun=mean, na.rm=T)
+  ui.age[is.na(ui.age)] <- 0
+  
+  return(as.numeric(ui.age))
+}
 
 
 #=======================================================================#
@@ -77,7 +112,6 @@ Isolation <- function(ui, fire.perim){
   # Load packages
   require(rgdal)
   require(rgeos)
-  require()
   require(matrixStats)
   
   # Calculate distance to fire perimeter for each UI
@@ -95,7 +129,6 @@ Isolation <- function(ui, fire.perim){
   
   return(s.min)
 }
-
 
 
 
@@ -135,7 +168,8 @@ Seedling <- function(ui, fire.perim) {
   ymax <- mround(ymax(fire.perim), 30, "max")
   r1 <- (xmax - xmin) / 30
   r2 <- (ymax - ymin) / 30
-  blank <- raster(resolution = 30, xmn = xmin, xmx = xmax, ymn = ymin, ymx = ymax, crs = projection(fire.perim), vals = 1)
+  blank <- raster(resolution = 30, xmn = xmin, xmx = xmax, ymn = ymin, 
+                  ymx = ymax, crs = projection(fire.perim), vals = 1)
   blank <- mask(blank, fire.perim)
   
   # Rasterize UI and perimeter
@@ -154,8 +188,244 @@ Seedling <- function(ui, fire.perim) {
   seed <- (-1 / (1 + 35 * exp(-0.016 * dist))) + 1
   
   # Assign seedling presence probability to UI
-  seed.sum <- extract(seed, ui.alloc, method = 'simple', small = T, fun = sum, na.rm = T, df = T, sp = T)
+  seed.sum <- extract(seed, ui.alloc, method = 'simple', small = T, 
+                      fun = sum, na.rm = T, df = T, sp = T)
   return(seed.sum@data$layer)
 }
 
 
+
+#=======================================================================#
+# Infrastructure
+#=======================================================================#
+
+# Number of federal structures (1 min)
+Infrastructure <- function(ui, fire.perim){
+  # Load packages
+  require(rgdal)
+  require(rgeos)
+  require(matrixStats)
+  
+  # Load buildings
+  fcc <- readOGR("Infrastructure/Intermediates/FCC_Point.shp")
+  nps <- readOGR("Infrastructure/Intermediates/NPS_Point.shp")
+  usfs <- readOGR("Infrastructure/Intermediates/USFS_Point.shp")
+  blm.p <- readOGR("Infrastructure/Intermediates/BLM_Point.shp")
+  blm.l <- readOGR("Infrastructure/Intermediates/BLM_Line.shp")
+  
+  # Test if UI contains a building
+  fcc.ui <- rowSums(gIntersects(fcc, ui, byid=T))
+  nps.ui <- rowSums(gIntersects(nps, ui, byid=T))
+  usfs.ui <- rowSums(gIntersects(usfs, ui, byid=T))
+  blm.ui <- rowSums(gIntersects(blm.p, ui, byid=T)) + 
+    rowSums(gIntersects(blm.l, ui, byid=T))
+  infra.ui <- fcc.ui + nps.ui + usfs.ui + blm.ui
+  
+  return(infra.ui)
+}
+
+
+#=======================================================================#
+# Critical Habitat
+#=======================================================================#
+
+# Critical habitat function (1.04 min)
+CritHabitat <- function(ui, fire.perim){
+  # Load packages
+  require(rgdal)
+  require(raster)
+  require(rgeos)
+  
+  # Load Critical Habitat data
+  hab <- readOGR("CriticalHabitat/CriticalHabitat.shp")
+  
+  # Identify UIs with Critical Habitat
+  int <- intersect(hab, ui) # 59 sec
+  
+  # Add score based upon type of species
+  h.score <- data.frame(listing_st= c("Endangered", "Threatened", 
+                                      "Recovery", "Proposed Endangered"),
+                        score.habitat= c(1,.5,.2,.2))
+  hab.ui <- merge(int@data[, c("ID", "listing_st")], h.score, by= "listing_st")
+  hab.ui <- merge(ui@data, hab.ui, by="ID", all=T)$score.habitat
+  hab.ui[is.na(hab.ui)] <- 0
+  return(hab.ui)
+}
+
+
+#=======================================================================#
+# Invasive plant species cover
+#=======================================================================#
+
+# Calculate proportion of invasive species cover
+Invasive <- function(ui, fire.perim){
+  # Load packages
+  require(rgdal)
+  require(rgeos)
+  require(raster)
+  
+  # Load invasive species data
+  inv <- readOGR("Invasive/Invasive.shp")
+  
+  # Clip data to fire perimeter
+  inv <- crop(inv, fire.perim)
+  
+  # Find area of total invasive species range (considering multiple species) within UI
+  inv <- gIntersection(ui, inv, byid = T)
+  ui.over <- over(inv, ui, returnList = F)
+  inv.area <- sapply(slot(inv, "polygons"), function(i) slot(i, "area"))
+  inv.df <- data.frame(ID = ui.over$ID,
+                       inv.area = inv.area)
+  inv.df <- aggregate(inv.area ~ ID, data = inv.df, FUN = sum)
+  
+  # Calculate proportion of each UI that is covered by invasive species
+  inv.df$ui.area <- sapply(slot(ui[ui@data$ID %in% inv.df$ID,], "polygons"), 
+                           function(i) slot(i, "area"))
+  inv.df$prop.inv <- inv.df$inv.area / inv.df$ui.area
+  inv.df <- merge(data.frame(ID = ui@data$ID), inv.df[, c(1,4)], all = T)
+  inv.df$prop.inv[is.na(inv.df$prop.inv)] <- 0
+  return(inv.df$prop.inv)
+}
+
+#=======================================================================#
+# Land cover relative abundance
+#=======================================================================#
+
+# Define land cover realative abundance function (3.78 mins)
+LandCover <- function(ui, fire.perim){
+  # Load packages
+  require(rgdal)
+  require(raster)
+  
+  # Identify state and load land cover data
+  pnw <- readOGR("PNW/pnw.shp")
+  state <- data.frame(intersect(pnw, fire.perim))
+  state <- toupper(substr(state$NAME_1, start = 1, stop = 2))
+  lcover.list <- paste0("lcover.", state)
+  for(i in 1:length(state)) {
+    assign(paste0(lcover.list[i]), raster(
+      paste0("LandCover/gaplandcov_", tolower(state[i]), "/gaplndcov_", state[i], ".img")))
+  }
+  
+  # Transform projections
+  fire.proj <- spTransform(fire.perim, projection(get(lcover.list[1])))
+  ui.proj <- spTransform(ui, projection(get(lcover.list[1])))
+  
+  # Clip land cover data to fire perimeter
+  clip.list <- paste0("lcover.", state, ".clip")
+  for(i in 1:length(state)) {
+    assign(clip.list[i], crop(get(lcover.list[i]), extent(fire.proj)))
+    assign(clip.list[i], round(mask(get(paste0(clip.list[i])), fire.proj)))
+  }
+  if(length(state) > 1){
+    assign("lcover.fire", merge(get0(clip.list[1]), get0(clip.list[2]), get0(clip.list[3])))
+  } else {
+    assign("lcover.fire", get(clip.list))
+  }
+  
+  # Get all land cover IDs present in UIs
+  lcover.ui <- mask(lcover.fire, ui.proj)
+  lcover.freq <- as.data.frame(freq(lcover.ui))
+  lcover.freq <- lcover.freq[!is.na(lcover.freq$value), ]
+  lcover.sort <- lcover.freq[order(lcover.freq$count), ]
+  
+  # Find mode land cover ID for each UI
+  #   Create mode (central tendency) function
+  Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+  }
+  
+  #   Extract raster values by polgon (UI) and calculate mode
+  r.mode <- extract(lcover.ui, ui.proj)
+  r.mode <- lapply(r.mode, function(x) x[!is.na(x)])
+  r.mode <-  lapply(r.mode, FUN=Mode)
+  r.mode <- as.numeric(r.mode)
+  
+  # Calculate relative abundance by area of landcover type
+  #   Assign mode land cover value to each UI polygon
+  r.uniq <- unique(r.mode)
+  r.area <- sapply(r.uniq, function(x) sum(values(lcover.ui) == x, na.rm = T))
+  r.rel <- r.area/sum(r.area)
+  r.abun <- data.frame(lcover.mode=r.uniq, r.rel=r.rel)
+  
+  # Assign score to each UI based on mode landcover type
+  ui.list <- data.frame(ID = ui@data$ID, r.mode)
+  ui.list <- merge(ui.list, r.abun, by.x="r.mode", by.y="lcover.mode")
+  ui.list <- ui.list[order(ui.list$ID), ]
+  
+  return(ui.list$r.rel)
+}
+
+
+#=======================================================================#
+# Table Mountain fire
+#=======================================================================#
+require(rgdal)
+setwd("S:/COS/PyroGeog/amartinez/Ranking/CriteriaData")
+fire.perim <- readOGR("UnburnedIsland/Table_perim.shp")
+ui <- readOGR("UnburnedIsland/Table_ui.shp")
+
+Times <- data.frame(Function = NULL, Time = NULL)
+
+s <- Sys.time()
+scores.df$CritHab <- CrtiHabitat(ui, fire.perim)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "CrtiHabitat", Time = difftime(e, s, units='mins')))
+Times$Time
+
+s <- Sys.time()
+scores.df$Infrstr <- Infrastructure(ui, fire.perim)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "Infrastructure", Time = difftime(e, s, units='mins')))
+Times$Time
+
+s <- Sys.time()
+scores.df$Invasiv <- Invasive(ui, fire.perim)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "Invasive", Time = difftime(e, s, units='mins')))
+Times$Time
+
+s <- Sys.time()
+scores.df$Isolatn <- Isolation(ui, fire.perim)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "Isolation", Time = difftime(e, s, units='mins')))
+Times$Time
+
+s <- Sys.time()
+scores.df$LandCvr <- LandCover(ui, fire.perim)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "LandCover", Time = difftime(e, s, units='mins')))
+Times$Time
+
+s <- Sys.time()
+scores.df$Seed <- Seedling(ui, fire.perim)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "Seedling", Time = difftime(e, s, units='mins')))
+Times$Time
+
+s <- Sys.time()
+scores.df$Size <- Size(ui)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "Size", Time = difftime(e, s, units='mins')))
+Times$Time
+
+s <- Sys.time()
+scores.df$StndAge <- StandAge(ui, fire.perim)
+e <- Sys.time()
+Times <- rbind(Times, data.frame(Function = "StandAge", Time = difftime(e, s, units='mins')))
+Times$Time
+
+Times$Time <- round(Times$Time, 2)
+Times
+sum(Times$Time)
+
+scores.df$StandAge <- as.numeric(scores.df$StandAge)
+Table <- ui
+Table@data <- merge(Table@data, scores.df, by = "ID")
+names(Table@data)[4:11] <- c("CritHab", "Infrstr", "Invasiv", "Isolatn", "LandCvr", "Seed", "StndAge", "Size")
+writeOGR(Table, dsn = "Output", layer = "TableMtn", driver = "ESRI Shapefile", overwrite_layer = T)
+
+### End ###
+summary(scores.df$Isolation)
+summary(scores.df$Isolation)
